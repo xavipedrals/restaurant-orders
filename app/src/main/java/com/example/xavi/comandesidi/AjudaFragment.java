d599c26a0349bfff2da09bf291086043779375bb
Xavier Pedrals <xavier.pedrals.camprubi@est.fib.upc.edu>
2016-01-09 19:04:36 +0100

    Help acabat. Commit pre early-entrega

--- app/src/main/java/com/example/xavi/comandesidi/AjudaFragment.java
+++ app/src/main/java/com/example/xavi/comandesidi/AjudaFragment.java
@@ -1,31 +1,33 @@
 package com.example.xavi.comandesidi;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.xavi.comandesidi.NovaComanda.MyItemRecyclerViewAdapter;
-import com.example.xavi.comandesidi.domini.ProductsContainer;
+import android.widget.ImageView;
 
 /**
  * Created by xavi on 07/01/16.
  */
 public class AjudaFragment extends Fragment {
-    //TODO: Fer editar nom, correu foto restarurant més opció netejar BD plats i BD comandes
-
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_help, container, false);
+        ImageView iconNovaComanda = (ImageView) view.findViewById(R.id.iconNovaComanda);
+        ImageView iconEditarPlats = (ImageView) view.findViewById(R.id.iconEditarPlats);
+        ImageView iconLlistarComandes = (ImageView) view.findViewById(R.id.iconLlistarComandes);
+        ImageView iconStocPlats = (ImageView) view.findViewById(R.id.iconStocProductes);
+        ImageView iconConfiguracio = (ImageView) view.findViewById(R.id.iconConfiguracio);
 
+        iconNovaComanda.setAlpha(138);
+        iconEditarPlats.setAlpha(138);
+        iconLlistarComandes.setAlpha(138);
+        iconStocPlats.setAlpha(138);
+        iconConfiguracio.setAlpha(138);
         return view;
     }
 }
